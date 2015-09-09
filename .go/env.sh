12e477b5b77620bfd379d1123ed7070082a1791a
Rodolfo Granata <warlock.cc@gmail.com>
2015-09-09 01:09:34 +0100

    moved sgrep to its own dir

--- .go/env.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-for p in ${GOPATH//:/ }; do
-  [ -d "$p" ] && ! [[ "$_export" =~ (^|:)$p(:|$) ]] && _export="$_export:$p"
-done
-echo export GOPATH="$_export":$(cd "$(dirname "$0")"; pwd)
-
-# vim: set sw=2 sts=2 : #
